7af1363437f6cbbc2bf2012d2e7cd8ed9d681d20
bbabbs <bbabbs@umich.edu>
2016-10-08 06:05:19 -0400

    adding input for person's attributes

--- app/src/main/java/com/example/nicholenguyen/fitnessapp/MainActivity.java
+++ app/src/main/java/com/example/nicholenguyen/fitnessapp/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.nicholenguyen.fitnessapp;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,6 +10,12 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,38 +23,45 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        Person p;
+
+        Spinner spinner_gender = (Spinner) findViewById(R.id.gender_spinner);
+        ArrayAdapter<String> adapter_gender = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, Gender);
+        adapter_gender.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner_gender.setAdapter(adapter_gender);
+
+        Spinner spinner_weight = (Spinner) findViewById(R.id.weight_spinner);
+        ArrayAdapter<String> adapter_weight = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, Weight);
+        adapter_weight.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner_weight.setAdapter(adapter_weight);
+
+
+        Spinner spinner_height = (Spinner) findViewById(R.id.height_spinner);
+        ArrayAdapter<String> adapter_height = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, Height);
+        adapter_height.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner_height.setAdapter(adapter_height);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editHeight = (EditText) findViewById(R.id.edit_height);
+        EditText editWeight = (EditText) findViewById(R.id.edit_weight);
+        EditText editAge = (EditText) findViewById(R.id.edit_age);
+        String[] message = new String[]{editHeight.toString(), editWeight.toString(), editAge.toString()};
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+        // Do something in response to button
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    static final String[] Gender = new String[]{"Male", "Female"};
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+    static final String[] Weight = new String[]{"lb", "kg"};
 
-        return super.onOptionsItemSelected(item);
-    }
+    static final String[] Height = new String[]{"in", "m"};
+
 }
---
+++ app/src/main/java/com/example/nicholenguyen/fitnessapp/Person.java
@@ -0,0 +1,17 @@
+package com.example.nicholenguyen.fitnessapp;
+
+/**
+ * Created by Blake on 08-Oct-16.
+ */
+
+public class Person {
+
+    String weight_measure;
+    int weight;
+    String height_measure;
+    int height;
+    Boolean gender;
+    int age;
+
+
+}
